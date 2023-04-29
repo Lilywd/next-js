fa933f09de753feb47b3cf63d29be7ff88768e57
lilywd <ndungulilianwanjiku@gmail.com>
2023-04-29 21:10:20 +0300

    trying out cpanel

--- shop/pages/index.js
+++ shop/pages/index.js
@@ -3,9 +3,21 @@ import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
 
+export  async function getStaticProps(){
+  const data =await fetch('http://localhost:3000/api/hello')
+
+
+  return{
+    props:{
+      data,
+    },
+  }
+}
+
 const inter = Inter({ subsets: ['latin'] })
 
-export default function Home() {
+export default function Home({data}) {
+  console.log (data)
   return (
     <>
       <Head>
